4293277fc15e4ae8d042047d42e808266f43cf49
darmilola <damilolaakinterinwa@gmail.com>
2021-08-31 16:22:49 +0100

    m

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ class Server{
          });
 
 
-         socket.on('match', function(receiverId,matchFirstname) {
+         socket.on('match', function(receiverId,matchFirstname,matchImageUrl) {
       
-             let  message = {"receiverId":receiverId,"matchFirstname":matchFirstname}
+             let  message = {"receiverId":receiverId,"matchFirstname":matchFirstname,"matchImageUrl":matchImageUrl}
              socket.to(users.get(receiverId)).emit("onMatch", "matched")
 
          }); 
@@ -92,6 +92,20 @@ class Server{
         socket.to(users.get(receiverId)).emit("message", message)
 
         })
+
+        });
+
+      
+       socket.on('notificationdetection', (receiverId,messageContent,senderName,senderImageUrl) => {
+
+        //create a message object 
+
+        let  message = {"message":messageContent, "receiverId":receiverId,"senderName":senderName,"senderImageUrl":senderImageUrl}
+
+       socket.to(users.get(receiverId)).emit("message", message)
+
+        })
+
 
         });
 }

ca64e4079e9afcd762c0b4a855bc80c62a576348
Akinterinwa damilola <31190237+darmilola@users.noreply.github.com>
2023-05-20 19:49:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,25 @@ class Server{
              socket.to(users.get(receiverId)).emit("onOnline", "online")
 
          });
+            
+              socket.on('match', function(receiverId,matchFirstname,matchImageUrl) {
+      
+             let  message = {"receiverId":receiverId,"matchFirstname":matchFirstname,"matchImageUrl":matchImageUrl}
+             socket.to(users.get(receiverId)).emit("onMatch",message)
+
+         }); 
+
+
+       socket.on('notificationdetection', (receiverId,messageContent,senderName,senderImageUrl) => {
+
+        //create a message object 
+
+        let  message = {"message":messageContent, "receiverId":receiverId,"senderName":senderName,"senderImageUrl":senderImageUrl}
+
+        socket.to(users.get(receiverId)).emit("notification", message)
+
+        });
+       
 
         
         socket.on('messagedetection', (receiverId,messageContent,messageType) => {

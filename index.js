7acac634ed3cc3d4bb56b632c3be6695f8e9efa0
Akinterinwa damilola <31190237+darmilola@users.noreply.github.com>
2023-05-20 13:14:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,6 @@ const { emit } = require("process");
 const socketio = require('socket.io');
 const users = new Map();
 
-/*const app = express()
-const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})*/
-
 class Server{
 
     constructor(){
@@ -102,9 +87,6 @@ class Server{
 
         });
 }
-  
-// Export the Express API
-//module.exports = this.app
 
 }
 

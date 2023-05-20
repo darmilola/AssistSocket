c1312c9501947c527b9ded328d1899ea4adabc3a
Akinterinwa damilola <31190237+darmilola@users.noreply.github.com>
2023-05-20 12:11:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,31 @@
-'use strict';
+const app = require('express')();
+2
+const { v4 } = require('uuid');
+3
 
-const express = require("express");
-const http = require('http');
-const socketio = require('socket.io');
-const users = new Map();
+4
+app.get('/api', (req, res) => {
+5
+  const path = `/api/item/${v4()}`;
+6
+  res.setHeader('Content-Type', 'text/html');
+7
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+8
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+9
+});
+10
 
+11
+app.get('/api/item/:slug', (req, res) => {
+12
+  const { slug } = req.params;
+13
+  res.end(`Item: ${slug}`);
+14
+});
+15
 
-
-
-class Server{
-
-    constructor(){
-        this.host = '0.0.0.0';
-        this.protocol = "https";
-        this.port = process.env.PORT || 3000;
-        this.app = express();
-        this.http = http.Server(this.app);
-        this.socket = socketio(this.http);
-        }
-
-   appExecute(){
-     
-        this.http.listen(this.port, this.host, () => {
-            console.log(`Listening on http:${this.host}:${this.port}`);
-        });
-
-        this.app.get('/', (req, res) => {
-
-         res.send('Chat Server is running on port 3000')
-        });   
-       
-       app.get('/ping', (req, res) => {
-        res.send('pong 🏓')
-    })
-
-        this.socket.on('connection', (socket) => {
-
-        console.log('user connected')
-
-            
-        socket.on('join', function(userId) {
-
-        users.set(userId, socket.id);
-
-        //socket.broadcast.emit('userjoinedChat',userNickname +" : has joined the chat ")
-        console.log('joined chat')
-       });
-
-
-         socket.on('disconnected', function(senderId,receiverId) {
-         
-             socket.to(users.get(receiverId)).emit("onOffline", "offline")
-             users.delete(senderId);
-         
-         });
-
-        socket.on('typing', function(receiverId) {
-      
-             socket.to(users.get(receiverId)).emit("onTyping", "typing")
-
-         }); 
-
-        socket.on('stoptyping', function(receiverId) {
-      
-             socket.to(users.get(receiverId)).emit("onStopTyping", "stoptyping")
-
-         }); 
-
-        socket.on('online', function(receiverId) {
-      
-             socket.to(users.get(receiverId)).emit("onOnline", "online")
-
-         });
-
-        
-        socket.on('messagedetection', (receiverId,messageContent,messageType) => {
-
-        //create a message object 
-
-        let  message = {"message":messageContent, "receiverId":receiverId,"messageType":messageType}
-
-        console.log(message)
-        console.log(users.get(receiverId))
-        console.log(receiverId)
-
-        socket.to(users.get(receiverId)).emit("message", message)
-
-        });
-        
-
-        });
-}
-
-}
-
-const app = new Server();
-app.appExecute();
+16
+module.exports = app;

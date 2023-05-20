9901f02dd7fbe6ce1c66b26f026e57bedad35a35
Akinterinwa damilola <31190237+darmilola@users.noreply.github.com>
2023-05-20 13:46:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,53 +27,51 @@ class Server{
         this.app.get('/', (req, res) => {
 
          res.send('Chat Server is running on port 3000')
-            
-            
-     });    
-       
-       
-           this.socket.on('connection', (socket) => {
+        });    
+
+        this.socket.on('connection', (socket) => {
 
         console.log('user connected')
 
-            
-        socket.on('join', function(userId) {
+    });
 
-        users.set(userId, socket.id);
+            
+        this.socket.on('join', function(userId) {
+
+        users.set(userId, this.socket.id);
 
         //socket.broadcast.emit('userjoinedChat',userNickname +" : has joined the chat ")
         console.log('joined chat')
        });
 
 
-         socket.on('disconnected', function(senderId,receiverId) {
+         this.socket.on('disconnected', function(senderId,receiverId) {
          
-             socket.to(users.get(receiverId)).emit("onOffline", "offline")
+            this.socket.to(users.get(receiverId)).emit("onOffline", "offline")
              users.delete(senderId);
          
          });
 
-        socket.on('typing', function(receiverId) {
+        this.socket.on('typing', function(receiverId) {
       
-             socket.to(users.get(receiverId)).emit("onTyping", "typing")
+            this.socket.to(users.get(receiverId)).emit("onTyping", "typing")
 
          }); 
 
-        socket.on('stoptyping', function(receiverId) {
+        this.socket.on('stoptyping', function(receiverId) {
       
-             socket.to(users.get(receiverId)).emit("onStopTyping", "stoptyping")
+            this.socket.to(users.get(receiverId)).emit("onStopTyping", "stoptyping")
 
          }); 
 
         socket.on('online', function(receiverId) {
       
-             socket.to(users.get(receiverId)).emit("onOnline", "online")
+            this.socket.to(users.get(receiverId)).emit("onOnline", "online")
 
          });
-         
 
         
-        socket.on('messagedetection', (receiverId,messageContent,messageType) => {
+         this.socket.on('messagedetection', (receiverId,messageContent,messageType) => {
 
         //create a message object 
 
@@ -83,15 +81,9 @@ class Server{
         console.log(users.get(receiverId))
         console.log(receiverId)
 
-        socket.to(users.get(receiverId)).emit("message", message)
+        this.socket.to(users.get(receiverId)).emit("message", message)
 
         });
-        
-
-        });
-            
-
-      
 }
 
 }
